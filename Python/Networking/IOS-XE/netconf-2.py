ef8025245086d9b59c521ffdccc2459f25fff03b
Daniel <danyurru92@gmail.com>
2024-08-16 19:40:00 -0400

    Adding router data netconf1-2 file

--- Python/Networking/IOS-XE/netconf-2.py
+++ Python/Networking/IOS-XE/netconf-2.py
@@ -2,8 +2,8 @@ from ncclient import manager
 # import logging
 # logging.basicConfig(level=logging.DEBUG)
 
-router = {"host": "ios-xe-mgmt-latest.cisco.com", "port": "10000",
-          "username": "developer", "password": "C1sco12345"}
+router = {"host": "sandbox-iosxr-1.cisco.com", "port": "830",
+          "username": "admin", "password": "C1sco12345"}
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
     for capability in m.server_capabilities:
